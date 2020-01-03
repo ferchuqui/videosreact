fa4592fd377b293ab95c59267f01ec41acec2973
ferchuqui <luisfernandofarji@hotmail.com>
2020-01-03 16:45:54 -0300

    Capitulo10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/Helloworld';
 
 ReactDom.render(<HelloWorld/>, document.getElementById(app))
