f1a45b59f60ac547afccad031dc9f50b73cefcb3
ferchuqui <luisfernandofarji@hotmail.com>
2020-01-03 23:25:49 -0300

    Capitulo 12

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import HelloWorld from './components/Helloworld';
-
-ReactDom.render(<HelloWorld/>, document.getElementById(app))
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
